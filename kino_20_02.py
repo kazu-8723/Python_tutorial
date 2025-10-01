95e193df8836df3008d33fb491b3300c423a7789
Matsuta Kazuya <mttkzy.8723@gmail.com>
2025-10-01 19:09:03 +0900

    csvファイル操作基礎

---
+++ kino_20_02.py
@@ -0,0 +1,17 @@
+import csv
+"""
+with open("test.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerow(["Yamada", 180, 70])
+"""
+"""
+with open("test.csv", "a") as f:
+    writer = csv.writer(f)
+    writer.writerows([["Ochiai", 200, 100], ["Suzuki", 175, 65], ["Aoki", 190, 105], ["Minato", 150, 40]])
+"""
+"""
+with open("test.csv", "r") as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print(row)
+"""
